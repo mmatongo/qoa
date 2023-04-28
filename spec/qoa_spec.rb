05aaa8f390cd483d9d688d16d25d09f592795963
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-04-28 08:12:10 +0000

    update specs
    Signed-off-by: Daniel M. Matongo <mmatongo_@hotmail.com>

--- spec/qoa_spec.rb
+++ spec/qoa_spec.rb
@@ -1,28 +1,37 @@
 require 'spec_helper'
 
-describe Qoa::NeuralNetwork do
-  let(:input_nodes) { 2 }
-  let(:hidden_nodes) { 2 }
-  let(:output_nodes) { 1 }
+RSpec.describe Qoa::NeuralNetwork do
+  let(:input_nodes) { 3 }
+  let(:hidden_nodes) { 4 }
+  let(:output_nodes) { 2 }
   let(:learning_rate) { 0.1 }
-  let(:nn) { Qoa::NeuralNetwork.new(input_nodes, hidden_nodes, output_nodes, learning_rate) }
+  let(:neural_network) { Qoa::NeuralNetwork.new(input_nodes, hidden_nodes, output_nodes, learning_rate) }
 
-  context 'XOR problem' do
-    it 'learns the XOR function' do
-      training_data = [
-        { inputs: [0, 0], targets: [0] },
-        { inputs: [0, 1], targets: [1] },
-        { inputs: [1, 0], targets: [1] },
-        { inputs: [1, 1], targets: [0] }
-      ]
-      10000.times do
-        data = training_data.sample
-        nn.train(data[:inputs], data[:targets])
-      end
-      expect(nn.query([0, 0]).first.round).to eq(0)
-      expect(nn.query([0, 1]).first.round).to eq(1)
-      expect(nn.query([1, 0]).first.round).to eq(1)
-      expect(nn.query([1, 1]).first.round).to eq(0)
+  describe '#initialize' do
+    it 'creates a neural network with the specified parameters' do
+      expect(neural_network.input_nodes).to eq(input_nodes)
+      expect(neural_network.hidden_nodes).to eq(hidden_nodes)
+      expect(neural_network.output_nodes).to eq(output_nodes)
+      expect(neural_network.learning_rate).to eq(learning_rate)
+    end
+  end
+
+  describe '#train' do
+    let(:inputs) { [0.5, 0.6, 0.7] }
+    let(:targets) { [0.25, 0.75] }
+
+    it 'does not raise an error' do
+      expect { neural_network.train(inputs, targets) }.not_to raise_error
+    end
+  end
+
+  describe '#query' do
+    let(:inputs) { [0.5, 0.6, 0.7] }
+
+    it 'returns an array of output node values' do
+      result = neural_network.query(inputs)
+      expect(result).to be_an(Array)
+      expect(result.size).to eq(output_nodes)
     end
   end
 end
