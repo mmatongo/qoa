0f6ce8fd5a890e66d69570297afab71e2467992c
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-04-28 13:27:42 +0000

    Refactor the Qoa::NeuralNetwork class with new learning rate and activation function.
    Signed-off-by: Daniel M. Matongo <mmatongo_@hotmail.com>

--- spec/neural_network_spec.rb
+++ spec/neural_network_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Qoa::NeuralNetwork do
   let(:input_nodes) { 2 }
   let(:hidden_nodes) { 4 }
   let(:output_nodes) { 1 }
-  let(:learning_rate) { 0.5 }
-  let(:nn) { Qoa::NeuralNetwork.new(input_nodes, hidden_nodes, output_nodes, learning_rate) }
-  let(:threshold) { 0.01 }
-  let(:mse) { Float::INFINITY }
+  let(:learning_rate) { 0.1 }
+  let(:activation_func) { :sigmoid }
+  let(:nn) { Qoa::NeuralNetwork.new(input_nodes, hidden_nodes, output_nodes, learning_rate, activation_func) }
 
   context 'XOR problem' do
     it 'learns the XOR function' do
@@ -15,7 +14,7 @@ RSpec.describe Qoa::NeuralNetwork do
         { inputs: [0, 0], targets: [0] },
         { inputs: [0, 1], targets: [1] },
         { inputs: [1, 0], targets: [1] },
-        { inputs: [1, 1], targets: [0] }
+        { inputs: [1, 1], targets: [0] },
       ]
       30_000.times do
         data = training_data.sample
