19a805aa667a46001386d52c44be859f160e028b
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-04-28 08:12:27 +0000

    add new tests
    Signed-off-by: Daniel M. Matongo <mmatongo_@hotmail.com>

---
+++ spec/neural_network_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe Qoa::NeuralNetwork do
+  let(:input_nodes) { 2 }
+  let(:hidden_nodes) { 4 }
+  let(:output_nodes) { 1 }
+  let(:learning_rate) { 0.5 }
+  let(:nn) { Qoa::NeuralNetwork.new(input_nodes, hidden_nodes, output_nodes, learning_rate) }
+  let(:threshold) { 0.01 }
+  let(:mse) { Float::INFINITY }
+
+  context 'XOR problem' do
+    it 'learns the XOR function' do
+      training_data = [
+        { inputs: [0, 0], targets: [0] },
+        { inputs: [0, 1], targets: [1] },
+        { inputs: [1, 0], targets: [1] },
+        { inputs: [1, 1], targets: [0] }
+      ]
+      30_000.times do
+        data = training_data.sample
+        nn.train(data[:inputs], data[:targets])
+      end
+      expect(nn.query([0, 0]).first.round).to eq(0)
+      expect(nn.query([0, 1]).first.round).to eq(1)
+      expect(nn.query([1, 0]).first.round).to eq(1)
+      expect(nn.query([1, 1]).first.round).to eq(0)
+    end
+  end
+end
