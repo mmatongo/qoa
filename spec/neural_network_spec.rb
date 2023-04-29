9faa5a764cc76d4eadf456b96decb7b911e295fc
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-04-29 21:37:24 +0200

    Reconfigure Neural Network architecture and modify training data
    - Modified the activation function from sigmoid to tanh
    - Reconfigured the Neural Network class arguments to include an array of hidden layers instead of the number of hidden nodes
    - Updated methods of generating training data for XOR, OR, and AND functions
    - Renamed some of the test data in the specs to reflect the new changes.

--- spec/neural_network_spec.rb
+++ spec/neural_network_spec.rb
@@ -5,21 +5,21 @@ RSpec.describe Qoa::NeuralNetwork do
   let(:hidden_nodes) { 4 }
   let(:output_nodes) { 1 }
   let(:learning_rate) { 0.1 }
-  let(:activation_func) { :sigmoid }
+  let(:activation_func) { :tanh }
   let(:dropout_rate) { 0.3 }
-  let(:nn) { Qoa::NeuralNetwork.new(input_nodes, hidden_nodes, output_nodes, learning_rate, dropout_rate) }
+  let(:nn) { Qoa::NeuralNetwork.new(input_nodes, [hidden_nodes], output_nodes, learning_rate, dropout_rate) }
 
   context 'XOR problem' do
     it 'learns the XOR function' do
-      training_data = [
+      xor_training_data = [
         { inputs: [0, 0], targets: [0] },
         { inputs: [0, 1], targets: [1] },
         { inputs: [1, 0], targets: [1] },
         { inputs: [1, 1], targets: [0] },
       ]
-      30_000.times do
-        data = training_data.sample
-        nn.train(data[:inputs], data[:targets])
+      50_000.times do
+        data = xor_training_data.sample
+        nn.train([data[:inputs]], [data[:targets]])
       end
       expect(nn.query([0, 0]).first.round).to be_within(0.1).of(0)
       expect(nn.query([0, 1]).first.round).to be_within(0.1).of(1)
@@ -39,9 +39,9 @@ RSpec.describe Qoa::NeuralNetwork do
     end
 
     it 'learns the OR function' do
-      30_000.times do
+      50_000.times do
         data = or_training_data.sample
-        nn.train(data[:inputs], data[:targets])
+        nn.train([data[:inputs]], [data[:targets]])
       end
 
       expect(nn.query([0, 0]).first.round).to be_within(0.1).of(0)
@@ -61,9 +61,9 @@ RSpec.describe Qoa::NeuralNetwork do
       end
 
       it 'learns the AND function' do
-        30_000.times do
+        50_000.times do
           data = and_training_data.sample
-          nn.train(data[:inputs], data[:targets])
+          nn.train([data[:inputs]], [data[:targets]])
         end
 
         expect(nn.query([0, 0]).first.round).to be_within(0.1).of(0)
--- spec/qoa_spec.rb
+++ spec/qoa_spec.rb
@@ -2,25 +2,25 @@ require 'spec_helper'
 
 RSpec.describe Qoa::NeuralNetwork do
   let(:input_nodes) { 3 }
-  let(:hidden_nodes) { 4 }
+  let(:hidden_layers) { [4, 5] }
   let(:output_nodes) { 2 }
   let(:learning_rate) { 0.1 }
   let(:dropout_rate) { 0.1 }
   let(:activation_func) { :sigmoid }
-  let(:neural_network) { Qoa::NeuralNetwork.new(input_nodes, hidden_nodes, output_nodes, learning_rate, dropout_rate, activation_func) }
+  let(:neural_network) { Qoa::NeuralNetwork.new(input_nodes, hidden_layers, output_nodes, learning_rate, dropout_rate, activation_func) }
 
   describe '#initialize' do
     it 'creates a neural network with the specified parameters' do
       expect(neural_network.input_nodes).to eq(input_nodes)
-      expect(neural_network.hidden_nodes).to eq(hidden_nodes)
+      expect(neural_network.hidden_layers).to eq(hidden_layers)
       expect(neural_network.output_nodes).to eq(output_nodes)
       expect(neural_network.learning_rate).to eq(learning_rate)
     end
   end
 
   describe '#train' do
-    let(:inputs) { [0.5, 0.6, 0.7] }
-    let(:targets) { [0.25, 0.75] }
+    let(:inputs) { [[0.5, 0.6, 0.7]] }
+    let(:targets) { [[0.25, 0.75]] }
 
     it 'does not raise an error' do
       expect { neural_network.train(inputs, targets) }.not_to raise_error
@@ -38,13 +38,13 @@ RSpec.describe Qoa::NeuralNetwork do
   end
 
   describe 'network output changes after training' do
-    let(:inputs) { [0.5, 0.6, 0.7] }
-    let(:targets) { [0.25, 0.75] }
+    let(:inputs) { [[0.5, 0.6, 0.7]] }
+    let(:targets) { [[0.25, 0.75]] }
 
     it 'changes output after training' do
-      initial_output = neural_network.query(inputs)
+      initial_output = neural_network.query(inputs.first)
       neural_network.train(inputs, targets)
-      new_output = neural_network.query(inputs)
+      new_output = neural_network.query(inputs.first)
 
       expect(new_output).not_to eq(initial_output)
     end
@@ -53,23 +53,37 @@ RSpec.describe Qoa::NeuralNetwork do
   describe 'activation function application' do
     it 'applies the correct activation function' do
       inputs = [0.5, 0.6, 0.7]
-      hidden_inputs = neural_network.send(:matrix_multiply, neural_network.instance_variable_get(:@weights_ih), inputs.map { |x| [x] })
-      hidden_outputs = neural_network.send(:apply_function, hidden_inputs, Qoa::ActivationFunctions.method(activation_func))
+      weights = neural_network.instance_variable_get(:@weights)
+      layer_outputs = [inputs.map { |x| [x] }]
 
-      expect(hidden_outputs).to be_an(Array)
-      expect(hidden_outputs.size).to eq(hidden_nodes)
+      weights.each do |w|
+        layer_inputs = neural_network.send(:matrix_multiply, w, layer_outputs.last)
+        layer_outputs << neural_network.send(:apply_function, layer_inputs, Qoa::ActivationFunctions.method(activation_func))
+      end
+
+      layer_outputs.shift # Remove input layer
+      layer_outputs.each.with_index do |layer_output, idx|
+        expect(layer_output).to be_an(Array)
+        expect(layer_output.size).to eq(hidden_layers[idx] || output_nodes)
+      end
     end
   end
 
   describe 'weight matrices dimensions' do
-    it 'has correct dimensions for the input-hidden weight matrix' do
-      expect(neural_network.instance_variable_get(:@weights_ih).size).to eq(hidden_nodes)
-      expect(neural_network.instance_variable_get(:@weights_ih).first.size).to eq(input_nodes)
-    end
+    it 'has correct dimensions for the weight matrices' do
+      weights = neural_network.instance_variable_get(:@weights)
 
-    it 'has correct dimensions for the hidden-output weight matrix' do
-      expect(neural_network.instance_variable_get(:@weights_ho).size).to eq(output_nodes)
-      expect(neural_network.instance_variable_get(:@weights_ho).first.size).to eq(hidden_nodes)
+      expect(weights.first.size).to eq(hidden_layers.first)
+      expect(weights.first.first.size).to eq(input_nodes)
+
+      hidden_layers.each_cons(2) do |l1, l2|
+        weight_matrix = weights[hidden_layers.index(l1) + 1]
+        expect(weight_matrix.size).to eq(l2)
+        expect(weight_matrix.first.size).to eq(l1)
+      end
+
+      expect(weights.last.size).to eq(output_nodes)
+      expect(weights.last.first.size).to eq(hidden_layers.last)
     end
   end
 
